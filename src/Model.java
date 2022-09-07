b8dcf3dad856f47a4ca730eadac6d71fc5e39fdb
NoErrorLikeMe <1508132612@qq.com>
2022-09-07 11:16:21 +0800

    test02

---
+++ src/Model.java
@@ -0,0 +1,20 @@
+public class Model {
+    private String person;
+    private String pet;
+
+    public String getPet() {
+        return pet;
+    }
+
+    public void setPet(String pet) {
+        this.pet = pet;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Model model=new Model();
+
+        model.setPerson("ss");
+        System.out.println(model.getPet());
+    }
+}
